fb0b4f820a595fee3240613fea8f6d22deb8562b
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-06-13 18:40:26 -0300

    Obtendo um usuário por id

--- src/main/java/com/jeffersonsilva/workshop_mongodb/services/UserService.java
+++ src/main/java/com/jeffersonsilva/workshop_mongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.jeffersonsilva.workshop_mongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jeffersonsilva.workshop_mongodb.domain.User;
 import com.jeffersonsilva.workshop_mongodb.repository.UserRepository;
+import com.jeffersonsilva.workshop_mongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,8 @@ public class UserService {
 		return userRepository.findAll();
 	}
 
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
 }
