af589e1f429cd3a1d0e99cd8cfafc1828d0aca71
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-06-14 13:19:00 -0300

    Obtendo um post por id

--- src/main/java/com/jeffersonsilva/workshop_mongodb/resouces/UserResource.java
+++ src/main/java/com/jeffersonsilva/workshop_mongodb/resouces/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.jeffersonsilva.workshop_mongodb.domain.Post;
 import com.jeffersonsilva.workshop_mongodb.domain.User;
 import com.jeffersonsilva.workshop_mongodb.dto.UserDTO;
 import com.jeffersonsilva.workshop_mongodb.services.UserService;
@@ -63,5 +64,11 @@ public class UserResource {
     	obj = userService.update(obj);
     	return ResponseEntity.noContent().build();
     }
+	
+    @GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 	
 }
